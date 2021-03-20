880c9a7777aba4ddad9391c6cb13d2149da2a201
gygasync <gygasync@gmail.com>
2021-03-20 21:52:05 +0100

    Add get Oldest task to task controller

--- src/controllers/task/index.js
+++ src/controllers/task/index.js
@@ -1,10 +1,8 @@
-const Task = require('../../models/task')
+const Tasks = require('../../models/task')
 
 exports.addTask = async function(req, res, next) {
     try {
-        console.log(req.body)
-        const exists = await Task.findOne({URL : req.body.URL})
-        console.log(exists)
+        const exists = await Tasks.findOne({URL : req.body.URL})
         if(exists) {
             res.status(303).send(exists)
         }
@@ -13,6 +11,20 @@ exports.addTask = async function(req, res, next) {
 
             await task.save()
             res.status(201).send(task)
+        }
+    } catch (e) {
+        res.status(400).send(e)
+    }
+}
+
+exports.getOldestTask = async function(req, res, next) {
+    try {
+        // Returns oldest task ordered by lastUpdated
+        var task = await Tasks.findOne({}, {}, { sort: { 'lastUpdated' : 1 }})
+        if(task) {
+            res.status(200).send(task)
+        } else {
+            res.status(404)
         }
     } catch (e) {
         res.status(400).send(e)
--- src/routes/task.js
+++ src/routes/task.js
@@ -3,5 +3,6 @@ const taskController = require('../controllers/task')
 const router = new express.Router()
 
 router.post('/task', taskController.addTask)
+router.get('/task/oldest', taskController.getOldestTask)
 
 module.exports = router
