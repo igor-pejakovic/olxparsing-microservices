117e7df2214a12cf6c7b5fdf20daa469d17a6885
gygasync <gygasync@gmail.com>
2021-03-20 17:37:18 +0100

    Add taskRouter to express server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const taskRouter = require('./src/routes/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
