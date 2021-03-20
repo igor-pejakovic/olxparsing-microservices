67c22d1b5a5410b02e09d264fdfb0872fc1ae913
gygasync <gygasync@gmail.com>
2021-03-20 17:34:25 +0100

    Add connection to mongodb

---
+++ src/db/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(`mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
