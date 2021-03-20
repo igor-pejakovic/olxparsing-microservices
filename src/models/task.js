47e6c1e831724ed4a89edaf478c3043ec947d6d8
gygasync <gygasync@gmail.com>
2021-03-20 18:21:11 +0100

    Change URL field in model task to be unique

--- src/models/task.js
+++ src/models/task.js
@@ -6,6 +6,7 @@ const Task = mongoose.model('Task', {
         type: String,
         required: true,
         trim: true,
+        unique: true,
         validate(value) {
             if(!validator.isURL(value)) {
                 throw new Error('URL is invalid.')
