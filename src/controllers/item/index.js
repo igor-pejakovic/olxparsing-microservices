b578c6504f2c7d5dbf15600b75c83ed13605693f
gygasync <gygasync@gmail.com>
2021-04-07 12:31:42 +0200

    Add dotenv sample file.

--- src/controllers/item/index.js
+++ src/controllers/item/index.js
@@ -21,22 +21,6 @@ exports.addOrUpdate = async function(itemData) {
     }
 }
 
-exports.getTopItems = async function(req, res, next) {
-    if(!req.query.count) {
-        res.status(400).send({message: 'Parameter count is required.'})
-        return
-    }
-    try {
-            var result = await topItems(Number.parseInt(req.query.count))
-            res.status(200).send(result)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-}
-
-async function topItems(count) {
-    return Items.find({}, {}, { sort: {'hits' : -1}}).limit(count)
-}
 
 exports.oldestCrawl = async function() {
     var item = await Items.findOne({}, {}, { sort: { 'lastCrawled' : 1 }})
